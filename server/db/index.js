e40332bdd09aa983e4761a8a9bece14c13a8435b
Yuvi 014 <devsupport2@ultrainfotech.net>
2020-11-06 23:56:26 +0530

    Trying it with IP

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://mongo:27018/vatchit', { useNewUrlParser: true })
+    .connect('mongodb://173.45.79.26:27018/vatchit', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
