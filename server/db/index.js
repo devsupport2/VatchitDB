33f3bcd7d8e84efbc4dc278fe489546631ff2128
devsupport2 <62893991+devsupport2@users.noreply.github.com>
2021-04-21 18:09:55 +0530

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://173.45.79.26:27018/vatchit', { useNewUrlParser: true }) //LiveServer
+    .connect('mongodb://173.45.79.18:27018/vatchit', { useNewUrlParser: true }) //LiveServer
     //.connect('mongodb://localhost:27017/vatchit', { useNewUrlParser: true }) //LocalServer
     .catch(e => {
         console.error('Connection error', e.message)
